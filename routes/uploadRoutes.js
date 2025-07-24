1a8868fe77eb267c4aa67ce3d662ffd6d394e95a
soham khot <sohamkhot75@gmail.com>
2025-07-24 12:32:54 +0530

    update1 backend

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -12,17 +12,22 @@ const upload = multer({
 
 router.post('/', (req, res) => {
   upload(req, res, (err) => {
+    console.log('Incoming upload request...');
     if (err) {
-      console.error(err);
+      console.error('Multer error:', err);
       return res.status(400).json({ error: err.message });
     }
 
+    console.log('Files received:', req.files);
+
     if (!req.files || req.files.length === 0) {
+      console.log('No files uploaded.');
       return res.status(400).json({ error: 'No files uploaded.' });
     }
 
     uploadController.handleUpload(req, res);
   });
 });
+
 
 module.exports = router;
